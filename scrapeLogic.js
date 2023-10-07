79668c3c673454174368bc91a4b2f199fb463657
Dolapo oluwole adefisayomi <devbyclace@gmail.com>
2023-10-07 11:07:40 +0100

    updated scrapping logic

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,29 +17,15 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    await page.goto("https://developer.chrome.com/");
+    await page.goto("https://example.com/");
+    const arr = []
+    const title = await page.$$eval('p', (doc) => {
+      const arr = []
+      doc.forEach(data => arr.push(data.textContent))
+      return arr
+  })
+  res.send({data: arr})
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
